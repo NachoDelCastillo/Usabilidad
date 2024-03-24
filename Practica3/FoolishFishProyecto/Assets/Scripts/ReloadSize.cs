dd887399722563d2335299f811a527f665e8e5b6
javics2002 <javiercanosalcedo@gmail.com>
2024-03-24 12:54:19 +0100

    P3

---
+++ Practica3/FoolishFishProyecto/Assets/Scripts/ReloadSize.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReloadSize : MonoBehaviour{
+    void Start()
+    {
+        
+    }
+    void changeSize(){
+      
+
+    }
+}
