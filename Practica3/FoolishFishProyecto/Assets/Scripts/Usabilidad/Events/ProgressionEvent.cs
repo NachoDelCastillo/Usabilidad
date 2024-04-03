f9430779b39d8a55a34b8af037044a0cdc97f061
Daniel <druiz07@ucm.es>
2024-04-03 13:49:56 +0200

    Avances en el tracker y comentarios de estructura

--- Practica3/FoolishFishProyecto/Assets/Scripts/Usabilidad/Events/ProgressionEvent.cs
+++ Practica3/FoolishFishProyecto/Assets/Scripts/Usabilidad/Events/ProgressionEvent.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class ProgressionEvent : TrackerEvent
 {
-    
+
+
+
+    //metodos to Json y to CSV
+    //private  string elmentoprogression1
+
+
+    //A parte de to CSV y toJson necesitasmos definir setters para los elementos 
+    //Por ejemnplo -> setElemento1(string elemento1);
+
+
+    //toCSV(std::unordered_map<CSVFields, std::string>& eventCSV)
+    //toJson()
+    //trackerEvent::toJson/toCsv 
+    // eventCSV[CSVFields::ElementoProgression1]= elementoProgression1
+    //json= TrackerEvent::toJson()
+    //json["ElementoProgression1"]= elmentoprogression1
 }
--- Practica3/FoolishFishProyecto/Assets/Scripts/Usabilidad/Events/ResourceEvent.cs
+++ Practica3/FoolishFishProyecto/Assets/Scripts/Usabilidad/Events/ResourceEvent.cs
@@ -5,4 +5,18 @@ using UnityEngine;
 public class ResourceEvent : TrackerEvent
 {
 
+    //metodos to Json y to CSV
+    //private  string resourcelement1
+
+
+    //A parte de to CSV y toJson necesitasmos definir setters para los elementos 
+    //Por ejemnplo -> setResElemento1(string Reselemento1);
+
+
+    //toCSV(std::unordered_map<CSVFields, std::string>& eventCSV)
+    //toJson()
+    //trackerEvent::toJson/toCsv 
+    // eventCSV[CSVFields::ElementoRecursos1]= resourcelement1
+    //json= TrackerEvent::toJson()
+    //json["ElementoRecursos1"]= resourcelement1
 }
