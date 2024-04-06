af06cf7923f06d40a5bf773d621469a65a1ad2bd
DiegoRol69 <drol@ucm.es>
2024-04-06 11:35:32 +0200

    jump events

---
+++ Practica3/FoolishFishProyecto/Assets/Scripts/Usabilidad/Events/Jump_Start.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump_Start : TrackerEvent
+{
+
+    int platformId;
+
+    public Jump_Start(int platformId): base()
+    {
+        this.platformId = platformId;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
